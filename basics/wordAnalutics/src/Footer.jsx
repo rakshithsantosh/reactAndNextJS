9fb8868f72dab8141fe81e6f6da5398d9d5ec3d7
rakshith_s <rakshithsantosh@gmail.com>
2025-01-07 18:58:31 +0530

    Word analytics project

---
+++ basics/wordAnalutics/src/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>&copy; Copyright by rakshith.all rights reserved</small>
+      <small>Last checked 15 days ago</small>
+    </footer>
+  );
+}
---
+++ basics/wordAnalutics/src/Textarea.jsx
@@ -0,0 +1,10 @@
+export default function Textarea() {
+  return (
+    <textarea
+      onChange={(e) => {}}
+      className="textarea"
+      placeholder="Enter your text here"
+      spellCheck="false"
+    />
+  );
+}
