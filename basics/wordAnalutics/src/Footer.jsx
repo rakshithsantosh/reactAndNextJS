60a21577a0232f2e164e103d9ebf1d2d48cb9bc0
rakshith_s <rakshithsantosh@gmail.com>
2025-01-08 20:36:40 +0530

    minor change

--- basics/wordAnalutics/src/Footer.jsx
+++ basics/wordAnalutics/src/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       <small>&copy; Copyright by rakshith.all rights reserved</small>
-      <small>Last checked 15 days ago</small>
+      <small>Last checked 25 days ago</small>
     </footer>
   );
 }
