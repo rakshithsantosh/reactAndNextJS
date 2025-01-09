07cf73c197a587a3ccc3770dbe8c6be8ec0bc187
rakshith_s <rakshithsantosh@gmail.com>
2025-01-09 20:43:00 +0530

    created components for sidebar

---
+++ trekBag/src/Components/Counter.jsx
@@ -0,0 +1,7 @@
+export default function Counter() {
+  return (
+    <p>
+      <b>1/3</b> items packed
+    </p>
+  );
+}
---
+++ trekBag/src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer>
+      <small>&copy; 2050 Copyright by rakshith.</small>
+      <p>
+        version <b>1.5</b>
+      </p>
+    </footer>
+  );
+}
