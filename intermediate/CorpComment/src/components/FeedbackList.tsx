d678139b7ec7a6ec24506ab41b9e0060992be5e3
rakshith_s <rakshithsantosh@gmail.com>
2025-01-16 12:31:05 +0530

    implemented fetching data and populaing the feeback list

--- intermediate/CorpComment/src/components/FeedbackList.tsx
+++ intermediate/CorpComment/src/components/FeedbackList.tsx
@@ -1,25 +1,64 @@
-import { TriangleUpIcon } from "@radix-ui/react-icons";
+import { useEffect, useState } from "react";
+import FeedbackItem from "./FeedbackItem";
+import Spinner from "./Spinner";
+import ErrorMessage from "./ErrorMessage";
 
 export default function FeedbackList() {
+  const [feedbackItems, setFeedbackItems] = useState([]);
+  const [isLoading, setiSLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const fetchFeedbackItems = async () => {
+    setiSLoading(true);
+
+    try {
+      const response = await fetch(
+        "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+      );
+
+      if (!response.ok) {
+        throw new Error();
+      }
+
+      const data = await response.json();
+      setFeedbackItems(data.feedbacks);
+    } catch (error) {
+      setErrorMessage("something went wrong");
+    }
+
+    setiSLoading(false);
+  };
+
+  useEffect(() => {
+    fetchFeedbackItems();
+
+    // setiSLoading(true);
+    // fetch(
+    //   "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+    // )
+    //   .then((response) => {
+    //     if (!response.ok) {
+    //       throw new Error();
+    //     }
+    //     return response.json();
+    //   })
+    //   .then((data) => {
+    //     setFeedbackItems(data.feedbacks);
+    //     setiSLoading(false);
+    //   })
+    //   .catch((error) => {
+    //     setErrorMessage("Something went Wrong");
+    //     setiSLoading(false);
+    //   });
+  }, []);
+
   return (
     <ol className="feedback-list">
-      <li className="feedback">
-        <button>
-          <TriangleUpIcon />
-          <span>643</span>
-        </button>
-        <div>
-          <p>B</p>
-        </div>
-        <div>
-          <p>CompanyName</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Perferendis ipsum necessitatibus iste voluptate aperiam qui.
-          </p>
-        </div>
-        <p>4d</p>
-      </li>
+      {isLoading && <Spinner />}
+      {errorMessage && <ErrorMessage message={errorMessage} />}
+      {feedbackItems.map((feedbackItem) => (
+        <FeedbackItem key={feedbackItem.id} feedbackItem={feedbackItem} />
+      ))}
     </ol>
   );
 }
